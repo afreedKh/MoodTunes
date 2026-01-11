f8666fcaf10915d724a401838c70e9263e3215af
afreedKh <www.afreedappi599@gmail.com>
2026-01-11 17:03:03 +0530

    chore: remove node_modules and dist from repository

--- backend/dist/infrastructure/config/app.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-const config = {
-    BASE_URL: process.env.BASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
-};
-exports.config = config;
